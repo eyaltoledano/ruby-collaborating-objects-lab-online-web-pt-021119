821c78fc2642795623515a8a7ef021441c416edf
Eyal Toledano <eyaltoledano@me.com>
2019-03-21 18:52:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,18 +10,14 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    # binding.pry
     artist, song, rest = filename.split(" - ")
-    # binding.pry
     new_instance = self.new(song)
     new_instance.artist_name=(artist)
     new_instance
-    # binding.pry
   end
 
   def artist_name=(artist_name)
     self.artist = Artist.find_or_create_by_name(artist_name)
-    # binding.pry
     self.artist.add_song(self)
   end
 end
